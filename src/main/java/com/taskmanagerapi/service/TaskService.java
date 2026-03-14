be0fd5edc64ce19b06f56fdf34d4eb671dc8973c
Colton Randall <coltonrandall.nz@gmail.com>
2026-03-15 07:35:31 +1100

    add delete logging

--- src/main/java/com/taskmanagerapi/service/TaskService.java
+++ src/main/java/com/taskmanagerapi/service/TaskService.java
@@ -2,13 +2,18 @@ package com.taskmanagerapi.service;
 
 import com.taskmanagerapi.model.Task;
 import com.taskmanagerapi.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.taskmanagerapi.model.TaskStatus.DELETED;
+
 @Service
 public class TaskService {
+    private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
 
     TaskRepository taskRepository = new TaskRepository();
 
@@ -27,6 +32,14 @@ public class TaskService {
     }
 
     public void deleteTask(String id){
-        taskRepository.delete(id);
+        taskRepository.findTaskById(id).ifPresent(task -> {
+            if (task.getStatus() == DELETED){
+                logger.info("Task {} is already deleted", task.getName());
+            } else {
+                task.setStatus(DELETED);
+                taskRepository.save(task);
+                logger.info("Task {} marked as DELETED", task.getName());
+            }
+        });
     }
 }
